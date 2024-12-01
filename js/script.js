b8e4de9b9874a10c7621af50646f69d215a2757b
Wesley DG <wesley.dagraca@gmail.com>
2024-12-01 17:52:57 -0500

    Ajout des fonctions dans le JS, correction pour le déclenchement de l'animation et nettoyage du html

--- js/script.js
+++ js/script.js
@@ -6,29 +6,45 @@ window.addEventListener("load", function () {
     );
 });
 
+// document.querySelectorAll(".timeline-block").onclick = function() {show(this);};
+document.querySelectorAll(".timeline-block").forEach((block) => {
+        block.addEventListener("click", (event) => {
+            show(block);
+            console.log("ok");
+        });
+});
 
-// Sélectionner le div que l'on souhaite observer
-const contents = document.querySelectorAll('.tool-list');
+function show(div) {
+    div.classList.toggle("show");
+    console.log("click");
+}
 
-// Créer un IntersectionObserver pour observer le positionnement du div
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        // Vérifier si le div est dans la zone centrale horizontale de l'écran
-        if (entry.isIntersecting) {
-            // Ajouter la classe 'animate' pour lancer l'animation
-            entry.target.classList.add('animate');
-        } else {
-            // Retirer la classe 'animate' si le div sort de la zone
-            entry.target.classList.remove('animate');
-        }
+function triggerAnim() {
+    // Sélectionner le div que l'on souhaite observer
+    const contents = document.querySelectorAll('.tool-list');
+
+    // Créer un IntersectionObserver pour observer le positionnement du div
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            // Vérifier si le div est dans la zone centrale horizontale de l'écran
+            if (entry.isIntersecting) {
+                // Ajouter la classe 'animate' pour lancer l'animation
+                entry.target.classList.add('animate');
+            } else {
+                // Retirer la classe 'animate' si le div sort de la zone
+                entry.target.classList.remove('animate');
+            }
+        });
+    }, {
+        root: null, // Observer sur la fenêtre de visualisation (viewport)
+        threshold: 0.5, // Déclenche l'animation si plus de 50% du div est visible
+        rootMargin: "0px 0px -10% 0px" // Détecte lorsque le div atteint la zone centrale horizontale de l'écran (val initiale bottom -50%)
     });
-}, {
-    root: null, // Observer sur la fenêtre de visualisation (viewport)
-    threshold: 0.5, // Déclenche l'animation si plus de 50% du div est visible
-    rootMargin: "0px 0px -50% 0px" // Détecte lorsque le div atteint la zone centrale horizontale de l'écran
-});
 
-// Observer les div
-contents.forEach(content => {
-    observer.observe(content);
-});
+    // Observer les div
+    contents.forEach(content => {
+        observer.observe(content);
+    });
+}
+
+triggerAnim();
